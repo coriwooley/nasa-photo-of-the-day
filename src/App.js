6fef8545655e7d08b19275dbc039c7ad89a69fd0
Cori Wooley <wooleycori@gmail.com>
2022-02-16 13:03:15 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import {API_KEY} from './Constants'
+import { API_KEY } from "./Constants";
 import defaultData from "./defaultData";
 import Media from "./content";
 
 function App() {
-  const [data, setData] = useState(defaultData)
+  const [data, setData] = useState(defaultData);
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    .then(res => {
-      setData(res.data);
-      console.log(res.data)
-    })
-    .catch(err => console.log(err))
-  }, [])
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
-      < Media content={data} mediaType={data.media_type} explanation={data.explanation} url={data.url} title={data.title} date={data.date}/>
+      <Media
+        content={data}
+        mediaType={data.media_type}
+        explanation={data.explanation}
+        url={data.url}
+        title={data.title}
+        date={data.date}
+      />
     </div>
   );
 }
